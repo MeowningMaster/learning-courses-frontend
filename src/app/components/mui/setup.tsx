64565ccd328a8b6a610feeb8b867d3fc748b9b95
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-11-13 21:11:38 +0200

    mui

---
+++ src/app/components/mui/setup.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { PropsWithChildren } from 'react'
+import { NextAppDirEmotionCacheProvider } from 'tss-react/next/appDir'
+import { theme } from './theme'
+
+export const MuiSetup = ({ children }: PropsWithChildren) => {
+    return (
+        <>
+            <CssBaseline />
+            <NextAppDirEmotionCacheProvider options={{ key: 'css' }}>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            </NextAppDirEmotionCacheProvider>
+        </>
+    )
+}
