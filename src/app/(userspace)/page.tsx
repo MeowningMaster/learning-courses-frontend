3f072a1df32887ca5b90e6317f60192b96e33ea4
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-11-30 23:42:44 +0200

    partial prerender 🪄

--- src/app/(userspace)/page.tsx
+++ src/app/(userspace)/page.tsx
@@ -2,8 +2,19 @@
 
 import { course } from '@/api'
 import { Typography } from '@mui/material'
+import { Suspense } from 'react'
 
-export default async function CoursesCatalog() {
+async function Catalog() {
   const courses = await course.getAll.call()
   return <>{JSON.stringify(courses)}</>
+}
+
+export default async function CoursesCatalog() {
+  return (
+    <>
+      <Suspense>
+        <Catalog />
+      </Suspense>
+    </>
+  )
 }
