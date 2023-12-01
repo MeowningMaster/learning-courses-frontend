021cf8755184916d0a2371c69f9bab8cb548cdae
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-12-01 02:20:27 +0200

    list fallback

--- src/app/(userspace)/page.tsx
+++ src/app/(userspace)/page.tsx
@@ -1,5 +1,6 @@
 import { course } from '@/api'
 import { CatalogCourseList } from '@/components/course/list'
+import { Skeleton } from '@mui/material'
 import { Suspense } from 'react'
 
 async function Context() {
@@ -7,10 +8,21 @@ async function Context() {
   return <CatalogCourseList list={courses} />
 }
 
+async function Fallback() {
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton variant="rounded" height={100} />
+      <Skeleton variant="rounded" height={80} />
+      <Skeleton variant="rounded" height={100} />
+      <Skeleton variant="rounded" height={80} />
+    </div>
+  )
+}
+
 export default function CoursesCatalog() {
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<Fallback />}>
         <Context />
       </Suspense>
     </>
