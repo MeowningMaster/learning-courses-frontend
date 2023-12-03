856a9e42c71f23643d310d082b59cc9177d788f8
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-12-03 20:29:49 +0200

    finish lesson

--- src/app/(userspace)/lesson/[id]/page.tsx
+++ src/app/(userspace)/lesson/[id]/page.tsx
@@ -1,7 +1,16 @@
 import * as api from '@/api'
+import { FinishButton } from '@/components/button/finish'
 import { PageFallback } from '@/components/fallback/page'
+import { getPermissions } from '@/utilities/permissions'
 import { Chip, Typography } from '@mui/material'
+import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
+
+async function finish({ id, chapterId }: { id: number; chapterId: number }) {
+  'use server'
+  await api.lesson.finishLesson.call({ params: { lessonId: id } })
+  redirect(`/chapter/${chapterId}`)
+}
 
 export async function Content(params: { id: number }) {
   const id = Number(params.id)
@@ -9,25 +18,28 @@ export async function Content(params: { id: number }) {
     api.lesson.get.call({ params: { lessonId: id } }),
   ])
 
+  const canOperate = getPermissions().course.operate
+
   return (
     <>
-      <div className="flex gap-4">
-        <Typography gutterBottom variant="h5" component="div">
-          {lesson.title}
-        </Typography>
-        {lesson.isFinished && (
-          <Chip label="Finished" variant="outlined" color="warning" />
-        )}
+      <div className="flex gap-4 justify-between">
+        <div className="flex gap-4">
+          <Typography gutterBottom variant="h5" component="div">
+            {lesson.title}
+          </Typography>
+          {lesson.isFinished && (
+            <Chip label="Finished" variant="outlined" color="warning" />
+          )}
+        </div>
+        <div className="flex gap-4">
+          {canOperate && !lesson.isFinished && (
+            <FinishButton object={lesson} finish={finish} />
+          )}
+        </div>
       </div>
       <Typography variant="body1" color="text.secondary">
         {lesson.description}
       </Typography>
-
-      {/* <div className="mt-10">
-        <Typography gutterBottom variant="h5" component="div">
-          Lessons
-        </Typography>
-      </div> */}
     </>
   )
 }
